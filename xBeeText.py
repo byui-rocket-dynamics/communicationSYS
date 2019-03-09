d94fcbfa2f6b5c7191b2c59f5f337e19ae32db8a
Amulek1416 <zcarico03@gmail.com>
2019-03-09 13:13:53 -0700

    Initial Commit

--- xBeeText.py
+++ xBeeText.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 #
 # Bitbang'd SPI interface with an 
-MCP3008 ADC device
+# MCP3008 ADC device
 # MCP3008 is 8-channel 10-bit analog to  
-digital converter
+# digital converter
 #  Connections are:
 #     CLK => SCLK  
 #     DOUT =>  MISO
@@ -22,11 +22,10 @@ def buildReadCommand(channel):
     singleEnded = 0x08
 
     # Return python list of 3 bytes
-    #   Build a python list using [1, 2, 
-3]
+    #   Build a python list using [1, 2, 3]
     #   First byte is the start bit
     #   Second byte contains single 
-ended along with channel #
+    #   ended along with channel #
     #   3rd byte is 0
     return []
     
@@ -41,16 +40,14 @@ bytes.
 def readAdc(channel):
     if ((channel > 7) or (channel < 0)):
         return -1
-    r = 
-spi.xfer2(buildReadCommand(channel))
+    r = spi.xfer2(buildReadCommand(channel))
     return processAdcValue(r)
         
 if __name__ == '__main__':
     try:
         while True:
             val = readAdc(0)
-            print "ADC Result: ", 
-str(val)
+            print ("ADC Result: ", str(val))
             time.sleep(5)
     except KeyboardInterrupt:
         spi.close() 
